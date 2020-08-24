26ec09755a9ed6888eff271618214bbe8aeb40c1
Tobias Lundell <tobias.lundell@ericsson.com>
2020-08-24 21:54:58 +0200

    Improve typing for passwordCheckConstants

--- client/src/components/auth/AuthForm/hooks/usePasswordCheck.ts
+++ client/src/components/auth/AuthForm/hooks/usePasswordCheck.ts
@@ -1,10 +1,8 @@
 import { CHECK_FAIL_REASON, CHECK_STATUS} from '../shared/passwordCheckConstants';
 
-type CHECK_FAIL_REASON_KEYS = keyof typeof CHECK_FAIL_REASON;
-
 const rules = new RegExp("^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.{8,})");
 
-const usePasswordCheck = (checkPassword: boolean, password: string, confirmPassword: string): [boolean, typeof CHECK_FAIL_REASON[CHECK_FAIL_REASON_KEYS]?] => {
+const usePasswordCheck = (checkPassword: boolean, password: string, confirmPassword: string): [boolean, CHECK_FAIL_REASON?] => {
   if (!checkPassword) return [CHECK_STATUS.OK];
 
   if (!password || !confirmPassword) return [CHECK_STATUS.FAILED, CHECK_FAIL_REASON.PASSWORD_MISSING]
--- client/src/components/auth/AuthForm/shared/passwordCheckConstants.ts
+++ client/src/components/auth/AuthForm/shared/passwordCheckConstants.ts
@@ -1,11 +1,8 @@
-const PASSWORD_MISSING: "password-missing" = "password-missing";
-const PASSWORD_MISMATCH: "passwords-do-not-match" = "passwords-do-not-match";
-const PASSWORD_COMPLEXITY_RULES: "password-does-not-satisfy-complexity-rules" = "password-does-not-satisfy-complexity-rules";
 
-export const CHECK_FAIL_REASON = {
-  PASSWORD_MISSING,
-  PASSWORD_MISMATCH,
-  PASSWORD_COMPLEXITY_RULES
+export enum CHECK_FAIL_REASON {
+  PASSWORD_MISSING = "password-missing",
+  PASSWORD_MISMATCH = "passwords-do-not-match",
+  PASSWORD_COMPLEXITY_RULES = "password-does-not-satisfy-complexity-rules"
 }
 
 export const CHECK_STATUS = {
