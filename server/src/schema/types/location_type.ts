fb6d5a3881fc330d0a701ecf921dabf7fb4b861e
Tobias Lundell <tobias.lundell@ericsson.com>
2021-02-09 23:19:40 +0100

    add events graphql query

---
+++ server/src/schema/types/location_type.ts
@@ -0,0 +1,13 @@
+import { IEvent } from '@crime-alert/shared/dist/models/event';
+import { GraphQLObjectType, GraphQLString } from 'graphql';
+import { AuthContext } from '../../services/auth/auth.types';
+
+const LocationType = new GraphQLObjectType<IEvent['location'], AuthContext>({
+  name: 'LocationType',
+  fields: () => ({
+    name: {type: GraphQLString},
+    gps: {type: GraphQLString}
+  })
+})
+
+export default LocationType
--- server/src/schema/types/root_query_type.ts
+++ server/src/schema/types/root_query_type.ts
@@ -1,7 +1,9 @@
-import { GraphQLObjectType } from 'graphql';
+import { GraphQLList, GraphQLObjectType } from 'graphql';
 import UserType from './user_type';
 import { AuthContext } from './../../services/auth/auth.types';
 import { IUser } from '@crime-alert/shared/dist/models/user';
+import EventType from './event_type';
+import { getEvents } from '../../services/events/events';
 
 const RootQueryType = new GraphQLObjectType<
   Omit<IUser, 'password'>,
@@ -13,6 +15,12 @@ const RootQueryType = new GraphQLObjectType<
       type: UserType,
       resolve(parentValue, args, req) {
         return req.user;
+      }
+    },
+    events: {
+      type: new GraphQLList(EventType),
+      resolve() {
+        return getEvents()
       }
     }
   }
